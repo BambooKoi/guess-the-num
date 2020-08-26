5bfd4cf7c296657cc3d8fb5e2e9b2063f423bf3f
BambooKoi <barberry.koi@gmail.com>
2020-08-25 23:37:27 -0400

    Added Reverse Gameplay

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 # Guess the Number
-A simple number guessing game. The computer will randomly choose a secret number. The secret number will be in a random range of 10-100, this means that some games will be easier or harder than the previous. Example, your first game could be guessing between '1 and 20', your next game could be between '1 and 50' or '1 and 10', and etc. You will have 6 tries to guess the secret number each time.
+A simple number guessing game. I have made two ways of playing this game, Normal and Reverse. See [Gameplay](#Gameplay) for more details.
 
-Notes: will update this ReadMe sometime later with a demo and basic guide.
+## Table of Contents
+- [Gameplay](#Gameplay)
+    - [Normal](#Normal)
+    - [Reverse](#Reverse)
+- [Built With](#Built-With)
+- [Running the Game](#Running-the-Game)
+- [License](#License)
+- [Donations (Optional)](#Donations-(Optional))
+
+## Gameplay
+There are two ways of playing this game, [Normal](#Normal) and [Reverse](#Reverse). Both are number guessing games, the difference is who's guessing.
+
+### Normal
+The computer will randomly choose a secret number. The secret number will be in a random range of 10-100, this means that some games will be easier or harder than the previous. Example, your first game could be guessing between '1 and 20', your next game could be between '1 and 50' or '1 and 10', and etc. You will have 6 tries to guess the secret number each time.
+
+![A Normal Guess the Number gameplay](demo_normal.gif)
+
+### Reverse
+The roles are reversed this time. You get to choose the secret number from a range of 1-50. You do not give the computer your number at any point. See if the computer can guess it in 6 tries.
+
+![A Reverse Guess the Number gameplay](demo_normal.gif)
+
+## Built With
+- [Python 3.8](python.org) with the help of the built-in modules:
+    - os
+    - random
+    - sys
+
+## Running the Game
+- Install Python 3.6 or newer.
+    - You can grab the latest copy from http://python.org.
+- Download the `guess_num.py`.
+- Run `guess_num.py`
+    - Follow the prompts on-screen
+    - Enjoy!
+
+## License ![GitHub](https://img.shields.io/github/license/BambooKoi/Hangman)
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Donations (Optional)
+Ko-fi is basically a virtual tip jar where you can support creatives for about the price of a cup of coffee.
+
+At this time, I'm not very active on Ko-fi nor do I offer any rewards. If you love my work and feel like supporting me, hit the button below to get started.
+
+Tipping is optional but I will appreciate any amount you choose to donate. Thank you (´• ω •`) ♡ !
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/I2I77G74)
--- guess_num.py
+++ guess_num.py
@@ -1,15 +1,44 @@
+# Guess the Number Game
+# Or
+# Reverse Guess the Number Game
+# Your choice
+# https://github.com/BambooKoi/guess-the-num
+
+import os
 import random
 import sys
-import os
 
 
-def game():
-    print('Guess the Number Game\n')
+def game_choice():
+    while True:
+        print('What version of Guess the Number would you like to play?')
+        print('Enter (quit) to exit.')
+        choice = input('(N)ormal or (R)everse?\n> ')
+        choice = choice.lower()
+        if choice == 'quit':
+            sys.exit()
+        elif choice == 'n':
+            os.system('cls')
+            while True:
+                guess()
+                again()
+        elif choice == 'r':
+            os.system('cls')
+            while True:
+                reverse()
+                again()
+        else:
+            print('Please choose N for normal or R for Reverse.\n')
+
+
+def guess():
+    print('Guess the Number Game')
+    print('A game where you try to guess the number that the computer picked.')
 
     top_range = random.randrange(10, 100, 5)
     secret_num = random.randint(1, top_range)
-    
-    print(f'I\'m thinking of a number betweeen 1 and {top_range}.')
+
+    print(f'\nI\'m thinking of a number betweeen 1 and {top_range}.')
     print('Enter (quit) to exit at any point.')
 
     guesses = 0
@@ -31,11 +60,11 @@ def game():
                 sys.exit()
             elif guess.isalpha():
                 print('\nThose are letters, please use numbers (e.g. 1, 2, 3...).\n')
-                game()
+                guess()
             else:
                 guess = int(guess)
                 break
-        
+
         os.system('cls')
         # Check if guess is cold/warm
         if str(guess) in guessed:
@@ -59,14 +88,14 @@ def game():
             elif guess > secret_num:
                 print(f'You\'re very close! {guess} is higher than my number.')
             else:
-            # Move on if guessed correctly or out of guesses
+                # Move on if guessed correctly or out of guesses
                 break
-            
+
             # Add to total number of guesses
             guesses += 1
             # Track previous guesses into a list
             guessed.append(str(guess))
-        
+
         # Display total guesses took & previous guesses
         print(f'\nYou\'ve used {guesses}/6 tries.')
         print(f'Numbers Guessed:', ', '.join(guessed))
@@ -85,19 +114,90 @@ def game():
         print(f'Numbers Guessed:', ', '.join(guessed))
 
 
+def reverse():
+    print('Reverse Guess the Number')
+    print('A game where the computer tries to guess your number instead.')
+
+    print('\nPick a number between 1 and 50. I will only guess whole numbers.')
+    input(f'Hit (Enter Key) when you are ready.')
+    os.system('cls')
+
+    max_range = 50
+    min_range = 1
+    guesses = 0
+
+    # Computer has 6 tries to guess.
+    while guesses < 6:
+        c_guess = random.randint(min_range, max_range)
+        # If the min_range and max_range doesn't make sense:
+        if (max_range - min_range) == 1:
+            print('Hey! I think you cheated.')
+            print(f'Your responses were: {responses}')
+            print(f'A whole number between {min_range} and {max_range} is impossible.')
+            break
+        elif max_range == min_range:
+            print(f'Well, the only answer left is {min_range}.')
+            print(f'Your responses were: {responses}')s
+            break
+        else:
+            pass
+        # Prevent the computer from choosing the same number:
+        while c_guess in guessed:
+            c_guess = random.randint(min_range, max_range)
+        else:
+            pass
+        print('Enter (quit) to exit at any point.')
+        answer = input(f'Is the number {c_guess}? (Y)es or (N)o.\n> ')
+        answer = answer.lower()
+        if answer == 'quit':
+            sys.exit()
+        elif answer == 'y':
+            if guesses == 0:
+                print('Whoo! I got it on the first guess.')
+            elif guesses <= 3:
+                print(f'Yay! It only took me {guesses + 1} tries to figure it out.')
+            else:
+                print(f'It took me {guesses + 1} tries.')
+            break
+        elif answer == 'n':
+            answer = input('Was my guess (H)igher or (L)ower than your number?\n> ')
+            if answer.startswith('l'):
+                min_range = c_guess
+                responses.append(f'{c_guess} is lower.')
+            elif answer.startswith('h'):
+                max_range = c_guess
+                responses.append(f'{c_guess} is higher')
+            else:
+                print(f'I did not understand "{answer}"')
+            guessed.append(c_guess)
+            guesses += 1
+            os.system('cls')
+        else:
+            print(f'I did not understand "{answer}"')
+
+        # if computer couldn't guess it in 6 attempts:
+        if guesses == 6:
+            print('I could not guess it in 6 tries. I give up.')
+            break
+
+
 def again():
-    play = input('\nPlay again? Y or N.\n> ')
+    print('\nPlay again?')
+    play = input('(Y)es, (N)o or (P)lay a different version.\n> ')
     if play.lower() == 'y':
         guessed.clear()
+        responses.clear()
         os.system('cls')
-    elif play.lower() == 'n':
+    elif play.lower() == 'quit' or play.lower() == 'n':
         sys.exit()
+    elif play.lower() == 'p':
+        game_choice()
     else:
-        print(f'I don\'t understand "{play}"? Please enter Y or N.')
+        print(f'I don\'t understand "{play}"? Please enter Y, N, or P.')
         again()
 
-guessed = []
 
-while True:
-    game()
-    again()
+guessed = []
+responses = []
+
+game_choice()
